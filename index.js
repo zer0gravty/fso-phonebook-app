ae8b71f88343de9305b63c048357b08110e815ed
Frank Vamos, Jr <fvamos@gmail.com>
2021-02-08 23:16:38 -0500

    Finished part3c

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-
+// custom
+const People = require('./models/phonebook');
+// setup
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' });
 }
@@ -12,92 +13,60 @@ const unknownEndpoint = (request, response) => {
 app.use(express.json());
 app.use(express.static('build'));
 app.use(cors());
-
+// logging
 morgan.token('post-body', (req) => {
     return Object.keys(req.body).length > 0 ? JSON.stringify(req.body) : '';
 });
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'));
-// const requestLogger = (request, response, next) => {
-    //     console.log('Method: ', request.method);
-    //     console.log('Path: ', request.path);
-    //     console.log('Body: ', request.body);
-    //     console.log('---');
-    //     next();
-    // }
-    // app.use(requestLogger);
-  
-let phonebook = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-    
+
+// routes
 app.get('/api/persons', (request, response) => {
-    response.json(phonebook);
+    People.find({}).then(people => response.json(people));
 });
 
 app.get('/info', (request, response) => {
-    const count = phonebook.length;
-    response.send(
-        `<p>Phonebook has info for ${count} people</p>
-        <p>${new Date().toISOString()}</p>`
+    People.countDocuments({}).then(count => {
+        return response.send(
+            `<p>Phonebook has info for ${count || 0} ${count === 1 ? 'person' : 'people'}</p>
+             <p>${new Date().toISOString()}</p>`
         )
     });
+});
         
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = phonebook.find( entry => entry.id === id);
-    return person ? response.json(person) : response.status(404).end();
+    const id = request.params.id;
+    People.findById(id).then(person => {
+        return person ? response.json(person) : response.status(404).end();
+    });
 });
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = phonebook.find( entry => entry.id === id);
-    if (!person) {
-        return response.status(400).end();
-    }
-    
-    phonebook = phonebook.filter( entry => entry.id !== id);
-    response.status(204).end();
+    const id = request.params.id;
+    People.findByIdAndDelete(id)
+        .then(result => {
+            return response.status(204).end();
+        })
+        .catch(error => {
+            return response.status(400).end();
+        });
 });
 
 app.post('/api/persons', (request, response) => {
-    const newId = Math.ceil(Math.random() * 1000000000);
     const person = request.body;
     if (!person.name || !person.number) {
         return response.status(400).json({ error: 'missing required value'});
     }
     
-    if (phonebook.some(entry => entry.name === person.name)) {
-        return response.status(400).json({ error: 'name already exists' });
-    }
+    // if (phonebook.some(entry => entry.name === person.name)) {
+    //     return response.status(400).json({ error: 'name already exists' });
+    // }
     
-    const newPerson = {
-        id: newId,
+    const newPerson = new People({
         name: person.name,
         number: person.number
-    }
-    
-    phonebook = phonebook.concat(newPerson);
-    response.json(newPerson);
+    });
+
+    newPerson.save().then(savedPerson => response.json(savedPerson));
 });
 
 app.use(unknownEndpoint);
---
+++ models/phonebook.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+const DB_URI = process.env.DB_URI;
+const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_URI}/${DB_NAME}?retryWrites=true&w=majority`;
+
+console.log('Connecting to MongoDB...');
+mongoose.connect(url, {
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+})
+.then(result => console.log('Connection successful.'))
+.catch(error => {
+    console.log(`Error connecting to MongoDB:\n\t${error}`);
+    process.exit(1);
+});
+
+const personSchema = mongoose.Schema({
+    name: String,
+    number: String
+});
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+module.exports = Person;
